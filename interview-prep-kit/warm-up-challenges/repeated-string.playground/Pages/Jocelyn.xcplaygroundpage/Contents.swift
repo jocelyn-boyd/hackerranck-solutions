6396db1762032f1b0a4295b69104288181c2f7d7
Jocelyn B <jocelynboyd@pursuit.org>
2021-06-17 09:40:58 -0400

    add warm up interview prep challenges

---
+++ interview-prep-kit/warm-up-challenges/repeated-string.playground/Pages/Jocelyn.xcplaygroundpage/Contents.swift
@@ -0,0 +1,15 @@
+//: [Previous](@previous)
+
+import Foundation
+
+/// Finds and prints the number of letter's in the first n of the infinite string.
+/// - Parameters:
+///   - s: a string to repeat
+///   - n: the number of characters to consider
+/// - Returns: the frequency of a in the substring
+func repeatedString(s: String, n: Int) -> Int {
+		// Write your code here
+
+	return Int()
+}
+
---
+++ interview-prep-kit/warm-up-challenges/repeated-string.playground/Pages/Question.xcplaygroundpage/Contents.swift
@@ -0,0 +1,5 @@
+import Foundation
+
+//: # Link https://www.hackerrank.com/challenges/repeated-string/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=warmup
+
+//: [Next](@next)
